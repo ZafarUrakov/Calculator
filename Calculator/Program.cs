fa1bae7c6818538e2e5e74fe43dcf0e7ec8c0f22
ZafarUrakov <goliburakov535@gmail.com>
2023-11-22 09:32:03 -0800

    INFRA: Init Project

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using Calculator.Classes;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("name: ");
+        string name = Console.ReadLine()!;
+
+        Calculate calculate = new Calculate(name);
+
+        Console.Write("first number: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine()!);
+
+        Console.Write("second number: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine()!);
+
+        calculate.Addition(firstNumber, secondNumber);
+    }
+}
